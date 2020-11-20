e638765af90f0efe622b0d880cb175b6c2078890
Anish Vijay <anishvijay333@gmail.com>
2020-11-20 20:45:46 +0530

    nn

--- make_audiobook.py
+++ make_audiobook.py
@@ -7,7 +7,7 @@ reader = PyPDF2.PdfFileReader(book)
 pages = reader.numPages
 print(pages)
 speaker = pyttsx3.init()
-for i in range(0,pages):
+for i in range(0, pages):
     page = reader.getPage(i)
     text = page.extractText()
     speaker.say(text)
