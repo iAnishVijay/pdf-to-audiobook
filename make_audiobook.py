b924c25657e785f348ce66c21809c45f1d755148
Anish Vijay <anishvijay333@gmail.com>
2020-11-20 20:26:09 +0530

    audiobook maker

---
+++ make_audiobook.py
@@ -0,0 +1,13 @@
+#this program makes audiobook from any .pdf file.
+import pyttsx3
+import PyPDF2
+book = open("class-11-Accountancy-part-1.pdf", 'rb')
+reader = PyPDF2.PdfFileReader(book)
+pages = reader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for i in range(0,pages):
+    page = reader.getPage(i)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
